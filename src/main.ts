ba1863af0d22bf0735af020f008de93518d8a715
Gustav Aaro <gustav.aaro@gmail.com>
2021-01-23 20:47:26 +0100

    Update port

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
     const app = await NestFactory.create(AppModule);
-    await app.listen(process.env.port || 3000);
+    console.log('Listening to', process.env.PORT);
+    await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
